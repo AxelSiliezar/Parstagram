46e14d679c7255e13cbc061a4166b590c3655a2b
Axel Siliezar <amartinezsiliezar@ucmerced.edu>
2021-03-08 23:06:21 -0800

    Part two completed

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -62,15 +62,17 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let image = info[.editedImage] as! UIImage
-        let size = CGSize(width: 399, height: 300)
-        let scaledImage = image.af_imageScaled(to: size)
-        
-        imageView.image = scaledImage
-        dismiss(animated: true, completion: nil)
-    }
-    
+    func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            let image = info[.editedImage] as! UIImage
+            
+            let size = CGSize(width: 300, height: 300)
+            let scaledImage = image.af_imageAspectScaled(toFill: size)
+            
+            imageView.image = scaledImage
+            
+            dismiss(animated: true, completion: nil)
+            
+        }
     /*
     // MARK: - Navigation
 
